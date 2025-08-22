cca45638df6cf94aade8653f25fbdae1acb1acc9
Qexat <43090614+qexat@users.noreply.github.com>
2025-08-22 15:50:19 +0200

    add `Or`

---
+++ lib/Or.ml
@@ -0,0 +1,26 @@
+type (_, _) t =
+  | Intro_left : 'a 'b. 'a -> ('a, 'b) t
+  | Intro_right : 'a 'b. 'b -> ('a, 'b) t
+
+let left : type a b. a -> (a, b) t = fun a -> Intro_left a
+let right : type a b. b -> (a, b) t = fun b -> Intro_right b
+let reflexivity : type a. a -> (a, a) t = fun a -> Intro_left a
+
+let symmetry : type a b. (a, b) t -> (b, a) t = function
+  | Intro_left a -> Intro_right a
+  | Intro_right b -> Intro_left b
+
+let identity_left : type b. (False.t, b) t -> b = function
+  | Intro_right b -> b
+
+let identity_right : type a. (a, False.t) t -> a = function
+  | Intro_left a -> a
+
+let associativity
+  : type a b c. ((a, b) t, c) t -> (a, (b, c) t) t
+  = function
+  | Intro_left (Intro_left a) -> Intro_left a
+  | Intro_left (Intro_right b) -> Intro_right (Intro_left b)
+  | Intro_right c -> Intro_right (Intro_right c)
+
+let commutativity = symmetry
---
+++ lib/Or.mli
@@ -0,0 +1,66 @@
+(** {1 Or} *)
+
+(** [Or] represents disjunction, denoted [∨]. *)
+
+(** The disjunction has two type parameters [a] and [b], with
+    two constructors {!Intro_left} and {!Intro_right} which
+    allow to construct a proof of [a ∨ b] either with a proof
+    of [a] or a proof of [b] respectively. *)
+
+(** [t] is the disjunction type. *)
+type (_, _) t =
+  | Intro_left : 'a 'b. 'a -> ('a, 'b) t
+  | Intro_right : 'a 'b. 'b -> ('a, 'b) t
+
+(** {2 Introduction rules} *)
+
+(** [left] states that no matter what [b] is, if we have a
+    proof of [a], then we can conclude [a ∨ b]. *)
+val left : 'a 'b. 'a -> ('a, 'b) t
+
+(** [right] states that no matter what [a] is, if we have a
+    proof of [b], then we can conclude [a ∨ b]. *)
+val right : 'a 'b. 'b -> ('a, 'b) t
+
+(** {2 Properties} *)
+
+(** {3 As a tolerance relation} *)
+
+(** {4 Reflexivity} *)
+
+(** [reflexivity] states that if we have [a], then we can
+    conclude [a ∧ a]. *)
+val reflexivity : 'a. 'a -> ('a, 'a) t
+
+(** {4 Symmetry} *)
+
+(** [symmetry] states that if we have [a ∨ b], then we can
+    conclude [b ∨ a].
+
+    See also {!val-commutativity}. *)
+val symmetry : 'a 'b. ('a, 'b) t -> ('b, 'a) t
+
+(** {3 As a logical operator} *)
+
+(** {4 Identity} *)
+
+(** [identity_left] states that if we have [⊥ ∨ b], then we
+    can conclude [b]. *)
+val identity_left : 'b. (False.t, 'b) t -> 'b
+
+(** [identity_right] states that if we have [a ∨ ⊥], then we can
+    conclude [a]. *)
+val identity_right : 'a. ('a, False.t) t -> 'a
+
+(** {4 Associativity} *)
+
+(** [associativity] states that if we have [(a ∨ b) ∨ c], then
+    we can conclude [a ∨ (b ∨ c)]. *)
+val associativity
+  : 'a 'b 'c.
+  (('a, 'b) t, 'c) t -> ('a, ('b, 'c) t) t
+
+(** {4 Commutativity} *)
+
+(** [commutativity] is the same as {!val-symmetry}. *)
+val commutativity : 'a 'b. ('a, 'b) t -> ('b, 'a) t
