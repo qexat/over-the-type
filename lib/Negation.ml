3fd344f882cef594de91567c68449f2e5a9c7e05
Qexat <43090614+qexat@users.noreply.github.com>
2025-08-22 13:54:52 +0200

    add reciprocal property for now we put it in [Negation] but once implication gets its own module we'll move it there

--- lib/Negation.ml
+++ lib/Negation.ml
@@ -1,4 +1,7 @@
 type -'a t = 'a -> False.t
 
+let reciprocal : type a b. (a -> b) -> b t -> a t =
+  fun a_implies_b not_b a -> not_b (a_implies_b a)
+
 let intro_double_negation : type a. a -> a t t = fun x f -> f x
 let ( ~~ ) = intro_double_negation
--- lib/Negation.mli
+++ lib/Negation.mli
@@ -9,6 +9,15 @@ type 'a t = 'a -> False.t
 
 (** {2 Properties} *)
 
+(** [reciprocal] states if we know that [a] implies [b], then
+    we can conclude that the negation of [b] implies the
+    negation of [a].
+
+    For example, we know that the rain makes the ground wet.
+    Therefore, if the ground is not wet, we can conclude that
+    it is not raining. *)
+val reciprocal : 'a 'b. ('a -> 'b) -> 'b t -> 'a t
+
 (** [intro_double_negation a] states that if we have [a], then
     we can conclude [¬¬a] (read: not not [a]). *)
 val intro_double_negation : 'a. 'a -> 'a t t
