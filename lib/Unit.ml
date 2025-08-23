c38fb11636381822ea6b5866039e3490118d6bfc
Qexat <43090614+qexat@users.noreply.github.com>
2025-08-23 09:50:28 +0200

    Revert "prove `Unit.uniqueness` with equality induction instead"
    This reverts commit 9d943e37e0402fef6c461307cdaec025c24ed689.
    
    The goal is in the hypotheses so it's probably not the best use
    case for it

--- lib/Unit.ml
+++ lib/Unit.ml
@@ -1,22 +1,9 @@
 [@@@warning "-65"]
 
-type tt = TT
+type tt
 type _ t = () : tt t
 
 let tt : tt t = ()
 
-module Uniqueness = struct
-  module Hypotheses = struct
-    type 'a ty = 'a t
-    type x = tt
-    type 'a _p = ('a, tt t) Eq.t
-    type 'a p = 'a _p constraint 'a = 'b ty
-
-    let px : x ty _p = Refl ()
-  end
-
-  module Conclusion = Eq.Induction.Make (Hypotheses)
-end
-
-let uniqueness : type a. a t -> (a t, tt t) Eq.t =
-  fun () -> Uniqueness.Conclusion.pa (Refl TT)
+let uniqueness : type a. a t -> (a t, tt t) Eq.t = function
+  | () -> Refl ()
