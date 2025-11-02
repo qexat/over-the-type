c8c049a3b17295eec1671b3cdff099ae85e69d13
Qexat <43090614+qexat@users.noreply.github.com>
2025-11-02 14:38:57 +0100

    not: fix `contrapositive` that was mistakenly named `reciprocal`

--- lib/Not.ml
+++ lib/Not.ml
@@ -1,6 +1,6 @@
 type -'a t = 'a -> False.t
 
-let reciprocal : type a b. (a -> b) -> b t -> a t =
+let contrapositive : type a b. (a -> b) -> b t -> a t =
   fun a_implies_b not_b a -> not_b (a_implies_b a)
 
 let double_introduction : type a. a -> a t t = fun x f -> f x
--- lib/Not.mli
+++ lib/Not.mli
@@ -9,14 +9,14 @@ type 'a t = 'a -> False.t
 
 (** {2 Properties} *)
 
-(** [reciprocal] states if we know that [a] implies [b], then
-    we can conclude that the negation of [b] implies the
+(** [contrapositive] states if we know that [a] implies [b],
+    then we can conclude that the negation of [b] implies the
     negation of [a].
 
     For example, we know that the rain makes the ground wet.
     Therefore, if the ground is not wet, we can conclude that
     it is not raining. *)
-val reciprocal : 'a 'b. ('a -> 'b) -> 'b t -> 'a t
+val contrapositive : 'a 'b. ('a -> 'b) -> 'b t -> 'a t
 
 (** [double_introduction a] states that if we have [a], then we
     can conclude [¬¬a] (read: not not [a]). *)
